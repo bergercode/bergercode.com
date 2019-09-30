3d2b7ea0c8769b6797d072ad79bb95770c4ad4cc
Daniel Berger <23620326+bergercode@users.noreply.github.com>
2019-09-30 11:57:18 +1000

    Updated to include animation of goodbye avatar and resizing

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 $(".navbar a").click(function (event) {
-    if($(this).attr("id")=="home-nav"){
+    if ($(this).attr("id") == "home-nav") {
         removeActive();
         $("#home-nav").addClass("active");
-    }else if($(this).attr("id")=="projects-nav"){
+    } else if ($(this).attr("id") == "projects-nav") {
         removeActive();
         $("#projects-nav").addClass("active");
-    }else if($(this).attr("id")=="contact-nav"){
+    } else if ($(this).attr("id") == "contact-nav") {
         removeActive();
         $("#contact-nav").addClass("active");
     }
 });
 
-function removeActive(){
+// $(window).scroll(function(){
+//     if($("body").height()<=($(window).height()+ $(window).scrollTop())){
+//       $("#bye-avatar").show();
+//     }
+//   });
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+        $("#bye-avatar").show();
+        // $("#bye-avatar").animate({left:"-=2px"});
+    }else{
+        $("#bye-avatar").hide();
+        // $("#bye-avatar").animate({left:"+=2px"});
+    }
+ });
+
+function removeActive() {
     $(".navbar a").removeClass("active");
 }
 
