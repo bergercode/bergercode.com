2de1dfc33ba327216a92a1d29f31b39cf524804e
Daniel Berger <23620326+bergercode@users.noreply.github.com>
2023-05-07 19:21:58 +1000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-$(".navbar a").click(function (event) {
+// Add an event listener to all navbar links
+$(".navbar a").click(function(event) {
+    // Check which link was clicked and add the "active" class to it
     if ($(this).attr("id") == "home-nav") {
         removeActive();
         $("#home-nav").addClass("active");
@@ -11,22 +13,23 @@ $(".navbar a").click(function (event) {
     }
 });
 
-// $(window).scroll(function(){
-//     if($("body").height()<=($(window).height()+ $(window).scrollTop())){
-//       $("#bye-avatar").show();
-//     }
-//   });
-  $(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+// Add a scroll event listener to the window
+$(window).scroll(function() {
+    // Check if the user has scrolled to the bottom of the page
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+        // Show the "bye-avatar" element
         $("#bye-avatar").show();
-        // $("#bye-avatar").animate({left:"-=2px"});
-    }else{
+        // You can uncomment the line below to animate the "bye-avatar" element
+        // $("#bye-avatar").animate({left: "-=2px"});
+    } else {
+        // Hide the "bye-avatar" element
         $("#bye-avatar").hide();
-        // $("#bye-avatar").animate({left:"+=2px"});
+        // You can uncomment the line below to animate the "bye-avatar" element
+        // $("#bye-avatar").animate({left: "+=2px"});
     }
- });
+});
 
+// A helper function to remove the "active" class from all navbar links
 function removeActive() {
     $(".navbar a").removeClass("active");
 }
-
